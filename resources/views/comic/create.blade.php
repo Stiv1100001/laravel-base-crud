ac725d12229af2de1f38dcbede13140dc6b46bce
Stiv1100001 <bichicchi.stefano@gmail.com>
2022-05-17 15:21:24 +0200

    done view

--- resources/views/comic/create.blade.php
+++ resources/views/comic/create.blade.php
@@ -1,1 +1,34 @@
 @extends('layouts.default')
+
+@section('title', 'Nuovo fumetto')
+
+@section('main')
+<main class="container my-5">
+    <h1>New comic</h1>
+    <form action="{{ route('comic.store') }}" class="d-flex flex-column">
+        @csrf
+        <label for="title">Title</label>
+        <input type="text" name="title" class="mb-3">
+
+        <label for="description">Description</label>
+        <textarea name="description" class="mb-3"></textarea>
+
+        <label for="thumb">Thumb</label>
+        <input type="text" class="mb-3" name="thumb">
+
+        <label for="type">Type</label>
+        <input type="text" class="mb-3" name="type">
+
+        <label for="series">Series</label>
+        <input type="text" class="mb-3" name="series">
+
+        <label for="price">Price</label>
+        <input type="number" class="mb-3" name="price" step="0.1" min="0">
+
+        <label for="sale_date">Sale Date</label>
+        <input type="text" class="mb-3" name="sale_date">
+
+        <input type="submit" class="btn btn-primary" value="Add">
+    </form>
+</main>
+@endsection
--- resources/views/comic/show.blade.php
+++ resources/views/comic/show.blade.php
@@ -1,13 +1,21 @@
 @extends('layouts.default')
-
 @section('title', "Comic")
-
 @section('main')
 <main class="my-5">
-    <div class="container d-flex">
+    <div class="container d-flex border border-2">
         <div class="img">
             <img src="{{ $comic['thumb']}}" alt="">
         </div>
+        <div class="info ms-2">
+            <h5> {{ $comic['title'] }}</h5>
+            <p>
+                {{ $comic['description']}}
+            </p>
+            <h6>Price: {{ $comic->price}} &dollar;</h6>
+            <h6>Type: {{ $comic->type}}</h6>
+            <h6>Series: {{ $comic->series}}</h6>
+
+        </div>
     </div>
 </main>
-@endcomponent
+@endsection
